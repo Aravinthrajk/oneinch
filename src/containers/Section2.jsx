09a702c24b1abc5cc1d2fb187b5112340ad8715c
baruitsolutions <barunitsolutions@hotmail.com>
2024-01-03 18:43:57 +0530

    Initial commit

---
+++ src/containers/Section2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { section2Content } from '../utils/content'
+import { Box } from '@mui/material'
+
+
+const {items} = section2Content
+
+const Section2 = () => {
+  return (
+    <Box></Box>
+  )
+}
+
+export default Section2
